7c49cabc185920d500c10e875cfc64a8b2002df2
Rochbouslama <roch.bouslama@esprit.tn>
2021-11-06 23:09:06 +0100

    edit join_worker.sh

--- join_worker.sh
+++ join_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubeadm join 192.168.1.16:6443 --token 86z7qe.4xfhmnh4pdd17kxl --discovery-token-ca-cert-hash sha256:e0464e0b71d29ff1c8ab5c5820de33a888fb5f878a1425a696dfd242811c53ac 
+kubeadm join 192.168.1.16:6443 --token j6x2hj.itl8221i65dzza27 --discovery-token-ca-cert-hash sha256:0a38a9f4b30ae9e5799e8f620b88bbce4030b2643386773f07a229fdc8a248b5 
